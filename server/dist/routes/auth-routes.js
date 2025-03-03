cc2910d56c8c10c408fe39e2aaf93dd27bd9d147
Kevin Noguera <noguera.ke@gmail.com>
2025-03-03 05:58:03 -0500

    implement user and ticket routes, authentication middleware, and database seeding

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { username } });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET_KEY || '', { expiresIn: '1h' });
+        return res.json({ token });
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            return res.status(500).json({ message: error.message });
+        }
+        return res.status(500).json({ message: 'An unknown error occurred' });
+    }
+};
+const router = Router();
+router.post('/login', login);
+export default router;
